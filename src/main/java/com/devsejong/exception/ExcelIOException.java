50c2592911b5fd4bf28b3ddbbeda07d10972b40a
SejongPark <devsejong@gmail.com>
2013-11-26 09:07:12 +0900

    .

---
+++ src/main/java/com/devsejong/exception/ExcelIOException.java
@@ -0,0 +1,11 @@
+package com.devsejong.exception;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: SejongPark
+ * Date: 2013. 11. 26.
+ * Time: 오전 1:30
+ * To change this template use File | Settings | File Templates.
+ */
+public class ExcelIOException {
+}
