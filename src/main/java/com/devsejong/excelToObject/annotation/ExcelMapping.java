3c30f1f3abf919ae60181568a2ba00588c37f3ca
yoe21c <yoe21c@gmail.com>
2014-01-03 06:36:52 +0900

    * 파일업로더 껍데기 * 클린코드에서 제시한 switch-case처리 예시

--- src/main/java/com/devsejong/excelToObject/annotation/ExcelMapping.java
+++
@@ -1,9 +0,0 @@
-package com.devsejong.excelToObject.annotation;
-
-import java.lang.annotation.*;
-
-@Target(ElementType.TYPE)
-@Retention(RetentionPolicy.RUNTIME)
-public @interface ExcelMapping {
-    //일단은.. 비워두자.
-}
